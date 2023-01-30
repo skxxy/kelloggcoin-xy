9fa31f1bd08d22aeef1dbc1dcaf2f557ac06de8e
skxxy <xiaoyixiong2023@u.northwestern.edu>
2023-01-30 21:42:20 +0000

    complete

--- practice.rb
+++ practice.rb
@@ -27,3 +27,36 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+ben_balance = 0 
+brian_balance = 0 
+evan_balance = 0 
+anthony_balance = 0 
+
+for transfer in blockchain
+  if transfer["to_user"] == "ben"
+    ben_balance = ben_balance + transfer["amount"]
+  elsif transfer["to_user"] == "brian"
+    brian_balance = brian_balance + transfer["amount"]
+  elsif transfer["to_user"] == "evan"
+    evan_balance = evan_balance + transfer["amount"]
+    transfer["to_user"] == "ben"
+  elsif transfer["to_user"] == "anthony"
+    anthony_balance = anthony_balance + transfer["amount"]
+  end
+
+  if transfer ["from_user"] =="ben"
+    ben_balance = ben_balance - transfer["amount"]
+  elsif transfer ["from_user"] =="brian"
+    brian_balance = brian_balance - transfer["amount"]
+  elsif transfer ["from_user"] =="evan"
+    evan_balance = evan_balance - transfer["amount"]
+  elsif transfer ["from_user"] =="anthony"
+    anthony_balance = anthony_balance - transfer["amount"]
+  end
+end
+puts "Ben's KelloggCoin balance is #{ben_balance}"
+puts "Brian's KelloggCoin balance is #{brian_balance}"
+puts "Evan's KelloggCoin balance is #{evan_balance}"
+puts "Anthony's KelloggCoin balance is #{anthony_balance}"
+
+
